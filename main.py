f2efdce09289aa20f0b07d8b82afa1cf58384d22
César Alves <Cesar.AlvesdeAzevedo-ext@ab-Inbev.com>
2020-12-10 21:28:19 -0300

    Creating function to write results

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import xlrd
+import xlwt
 import my_asset_class as Asset
+import os.path as path
+from os import listdir
 
 operations = []
 
@@ -11,7 +14,7 @@ def add_new_operation(new_op, operation_type):
                 new_qnt = op.quant + new_op.quant
                 new_price = ((op.quant * op.price) + (new_op.quant * new_op.price)) / new_qnt
                 op.quant = new_qnt
-                op.price = float(int(new_price*1000)/1000)
+                op.price = float(int(new_price * 1000) / 1000)
                 return
             elif operation_type == 'VENDIDA':
                 new_qnt = op.quant - new_op.quant
@@ -20,8 +23,8 @@ def add_new_operation(new_op, operation_type):
     operations.append(new_op)
 
 
-def perform():
-    file_b3 = xlrd.open_workbook('files/compras_b3.xls')
+def read_excel_file(file):
+    file_b3 = xlrd.open_workbook(file)
     sheet = file_b3.sheet_by_index(0)
 
     found_buy_title = False
@@ -45,7 +48,7 @@ def perform():
 
         if starting_asset_read:
             if value != '':
-                asset = str(sheet.cell(row, 3).value)
+                asset = str(sheet.cell(row, 3).value).strip()
                 data_compra = str(sheet.cell(row, 10))
                 qnt_buy = int(sheet.cell(row, 18).value)
                 qnt_sell = int(sheet.cell(row, 24).value)
@@ -53,6 +56,9 @@ def perform():
                 average_sell_prince = str(sheet.cell(row, 43).value)
                 qnt_liquida = str(sheet.cell(row, 49).value)
                 operation_type = str(sheet.cell(row, 54).value)
+
+                if asset[-1] == 'F':
+                    asset = asset[0:len(asset) - 1]
 
                 qnt = qnt_sell if operation_type == 'VENDIDA' else qnt_buy
                 prince = average_sell_prince if operation_type == 'VENDIDA' else average_buy_prince
@@ -67,10 +73,41 @@ def asset(op):
     return op.asset
 
 
+def write_excel_file():
+    xls = xlwt.Workbook()
+    sheet = xls.add_sheet('ASSETS')
+
+    index = 0
+    sheet.write(index, 0, 'ASSET')
+    sheet.write(index, 1, 'QNT')
+    sheet.write(index, 2, 'PRICE')
+
+    for operation in operations:
+        index += 1
+        sheet.write(index, 0, operation.asset)
+        sheet.write(index, 1, operation.quant)
+        sheet.write(index, 2, operation.price)
+
+    xls.save('output.xls')
+
+
+def perform():
+    print("READING EXCEL")
+    for file in listdir('files'):
+        file_path = 'files/' + file
+        if path.isfile(file_path):
+            read_excel_file(file_path)
+
+    print('SORTING')
+    operations.sort(key=asset)
+
+    print("WRITING DATA")
+    write_excel_file()
+
+    print('FINISHED WITH SUCCESS')
+
+
 if __name__ == '__main__':
     perform()
-    operations.sort(key=asset)
-    for operation in operations:
-        operation.print()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
